9c6041a2b39fb7ad2ac2e9fb32985ef8fb81d6d1
gangivinay <gangivinay557@gmail.com>
2026-08-28 14:38:04 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (32.60%) - LeetSync

--- 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.c
+++ 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.c
@@ -1,16 +1,27 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int minOperations(int* nums, int numsSize, int k) {
-    long long totalSum = 0; // Using long long prevents numbers from getting too large and breaking
-    
-    // Step 1: Calculate the total sum of all elements in the array
+int sum =0;
     for (int i = 0; i < numsSize; i++) {
-        totalSum += nums[i];
+        sum += nums[i]; 
     }
-    
-    // Step 2: Find the remainder using the modulo (%) operator
-    int operationsNeeded = totalSum % k;
-    
-    // Step 3: Return the final result
-    return operationsNeeded;
+
+return sum % k;
 }
+
+
+
+
+// #include <stdio.h>
+
+// int minOperations(int* nums, int numsSize, int k) {
+//     long long sum = 0;
+    
+ 
+//     for (int i = 0; i < numsSize; i++) {
+//         sum += nums[i];
+//     }
+    
+   
+//     return sum % k;
+// }
