321e2d031f27540d24f15a7171331015cffa1581
gangivinay <gangivinay557@gmail.com>
2026-08-31 21:38:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (87.76%) - LeetSync

---
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int minimumOperations(int* nums, int numsSize) {
+    int count =0;
+   
+    for(int i=0;i<numsSize;i++){
+        if(nums[i] % 3){
+        count ++;
+        }
+    }
+  return count;
+}
