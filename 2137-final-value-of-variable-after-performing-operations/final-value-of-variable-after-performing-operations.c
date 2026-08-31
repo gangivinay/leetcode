85c161a25cd5adc547f59519a935cb33181ed293
gangivinay <gangivinay557@gmail.com>
2026-08-31 21:57:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (42.36%) - LeetSync

---
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int finalValueAfterOperations(char** operations, int operationsSize) {
+    int x =0;
+  for(int i=0;i<operationsSize;i++){
+    if(operations[i][1] == '+' ){
+        x++;
+    }else{
+        x--;
+    }
+  }  
+  return x;
+}
