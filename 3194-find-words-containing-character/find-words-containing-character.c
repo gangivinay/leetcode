ff4d63f51d7c7af63b51fe6965e68a133dfc6e19
gangivinay <gangivinay557@gmail.com>
2026-09-01 22:16:50 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (100.00%) - LeetSync

---
+++ 3194-find-words-containing-character/find-words-containing-character.c
@@ -0,0 +1,47 @@
+// #include <stdlib.h>
+
+// int* findWordsContaining(char** words, int wordsSize, char x, int* returnSize) {
+//     int* ans = malloc(wordsSize * sizeof(int));
+//     *returnSize = 0;
+
+//     for (int i = 0; i < wordsSize; i++) {
+//         for (int j = 0; words[i][j] != '\0'; j++) {
+//             if (words[i][j] == x) {
+//                 ans[*returnSize] = i;
+//                 (*returnSize)++;
+//                 break;
+//             }
+//         }
+//     }
+
+//     return ans;
+// }
+
+
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+ #include<stdio.h>
+ #include<stdlib.h>
+
+int* findWordsContaining(char** words, int wordsSize, char x, int* returnSize) {
+ int* ans = malloc(wordsSize * sizeof(int));  
+ *returnSize =0; 
+for(int i=0;i<wordsSize;i++){
+for(int j=0; words[i][j] !='\0';j++){
+    if(words[i][j]==x){
+      ans[*returnSize] = i;
+        (*returnSize)++;
+        break;
+    }
+}
+    
+}
+
+return ans;
+
+}
+
